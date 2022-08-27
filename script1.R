2d6410765eb456ee6842a31015ce61a354b00484
Jazmín Cabrera Silguera <jazmin.cabrera@pucp.edu.pe>
2022-08-27 18:10:14 -0500

    première git

---
+++ script1.R
@@ -0,0 +1,21 @@
+#set up
+
+install.packages("pacman")
+pacman::p_load(tidyverse, haven)
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(devtools, usethis, tidyverse)
+
+use_git_config(user.name = "Jazmín Cabrera Silguera", user.email = "jazmin.cabrera@pucp.edu.pe")
+
+usethis::use_git()
+#not now y luego yeah
+
+#generar token para controlar github desde r
+usethis::create_github_token(description = "jazcabreras")
+
+usethis::edit_r_environ()
+
+data("mtcars")
+str(mtcars$disp)
+str(mtcars)
